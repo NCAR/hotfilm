4a7afc50df4267cd31d0c39548f7216425af59cf
Gary Granger <granger@ucar.edu>
2023-06-19 11:08:20 -0600

    convert labjack calls to a class, use 4 channels

--- README.md
+++ README.md
@@ -1,112 +1,28 @@
-= Hot Film Sampling Test =
+# Hot Film Sampling with LabJack
 
-== LabJack U6 ==
+The plan is to use a [Labjack
+T7](https://labjack.com/pages/support?doc=%2Fdatasheets%2Ft-series-datasheet%2F)
+to sample at least 4 hotfilm AD channels at up to 2 KHz and record the
+channels to disk in NIDAS dat archive format.
 
-The LabJack U6 connected to fl1 USB shows up like this:
+The program will run separately from the NIDAS `dsm` process, and it will be
+built outside of the NIDAS source tree, so it can be built easily on the DSM3
+Pi against buster branch but also on eol servers against nidas master branch.
 
-```sh
-daq@fl1:~ $ lsusb
-...
-Bus 001 Device 011: ID 0cd5:0006 LabJack Corporation 
-```
+## LabJack T7 C++ interface
 
-Installed LabJackPython for python3 on fl1:
+The recommended library interface for the T7 is the LJM C/C++ library:
 
-```sh
-daq@fl1:~/LabJackPython $ python3
-Python 3.7.3 (default, Jan 22 2021, 20:04:44) 
-[GCC 8.3.0] on linux
-Type "help", "copyright", "credits" or "license" for more information.
->>> import u6
->>> d = u6.U6()
->>> d.config
-d.configIO(          d.configTimerClock(  d.configU6(          
->>> d.configU6()
-{'FirmwareVersion': '1.43', 'BootloaderVersion': '6.15', 'HardwareVersion': '2.00', 'SerialNumber': 360013841, 'ProductID': 6, 'LocalID': 1, 'VersionInfo': 4, 'DeviceName': 'U6'}
->>> 
-```
-
-Firmware version for this labjack is 1.43, while latest on
-https://labjack.com/support/firmware/u6 is 1.45.  Firmware has to be
-upgraded from Windows, but upgrade probably isn't needed.
-https://labjack.com/support/software/applications/ud-series/ljselfupgrade
-
-This page:
-
- - https://labjack.com/support/software/installers/exodriver
-
-suggests using the LJM Library for C/C++ for the T7 and T4, or the ljacklm for the U12,
-rather than the lower-level exodriver (liblabjackusb).
-
- - https://labjack.com/support/faq/what-driverlibrary-should-i-use-my-labjack
-
-I think we want "stream mode":
-
- - https://labjack.com/support/app-notes/maximum-command-response
- - https://labjack.com/support/datasheets/u6/operation/stream-mode
-
-According to this, resolution index 1 (same as 0) is 16 bits, which should be
-good enough.  The hot film anemometers range from 0-5V, so we would use the
-gain setting of 1 for range +-10V.
-
- - https://labjack.com/support/datasheets/u6/appendix-b
-
-According to the stream mode table, the U6 can scan 4 channels at 12.5 KHz,
-for resolution index 1, gain 1, range +-10V.  It sounds like all four channels
-are scanned together, so that means we can reference all channels to the same
-time.
-
-The stream buffer size is 984 16-bit samples, so the buffer can hold 984/4=246
-4-channel scans.  That means the buffer has to be read at least every 246/2KHz
-seconds, or .1 seconds.  However, I'm not sure the LabJackPython API allows
-the amount to be read to be changed.
-
-Perhaps there are logic and features in the Diamond and NCAR A/D code in
-NIDAS which can be used for the LabJack, especially for optimizing buffer
-levels and reporting on dropouts.
-
-LabJack stream mode says it will fill in samples with -9999 if the buffer
-overflows.  I'm guessing that happens on the host side software side?  It
-does not overwrite a full buffer, it will discard samples while the buffer
-is full.
-
-In the simple test script, every read returns at most 1200 samples.  It
-returns 1200 for one channel, or 600 for each of two channels, 400 for each of
-3, or 300 for each of 4.  I don't know why I was seeing the incomplete before,
-unless it had something to do with the scan frequency.  Maybe 10 Hz was too
-slow to fill the packet with all complete scans.
-
-1200 samples requires 2*1200 + 14 = 2414 bytes, but the python script
-is reporting 3072 bytes of raw data in the read, for 48 packets, or 64 bytes per packet.
-4 channels * 2 bytes/channel * 8 scans is 64 bytes, 32 samples, but the number
-of values processed from each channel is 300.
-
-```sh
-INFO:hotfilm.py:read: errors=0, numPackets=48, missed=0, firstPacket=16, bytes=3072, bpp=64
-INFO:hotfilm.py:ain0: n=300,nmissing=0,avg=-10.59, ain1: n=300,nmissing=0,avg=-10.59, ain2: n=300,nmissing=0,avg=-10.59, ain3: n=300,nmissing=0,avg=-10.59
-INFO:hotfilm.py:read: errors=0, numPackets=48, missed=0, firstPacket=64, bytes=3072, bpp=64
-INFO:hotfilm.py:ain0: n=300,nmissing=0,avg=-10.59, ain1: n=300,nmissing=0,avg=-10.59, ain2: n=300,nmissing=0,avg=-10.59, ain3: n=300,nmissing=0,avg=-10.59
-INFO:hotfilm.py:read: errors=0, numPackets=48, missed=0, firstPacket=112, bytes=3072, bpp=64
-INFO:hotfilm.py:ain0: n=300,nmissing=0,avg=-10.59, ain1: n=300,nmissing=0,avg=-10.59, ain2: n=300,nmissing=0,avg=-10.59, ain3: n=300,nmissing=0,avg=-10.59
-```
-
-bpp is 64, which is `14+(stream_samples_per_packet*2)` according to
-`processStreamData()`, so that makes sense since samples_per_packet is 25.
-25*48 = 1200.
+- [Software info.](https://labjack.com/pages/support?doc=%2Fsoftware-driver%2Fexample-codewrappers%2Fcc-for-ljm-windows-mac-linux%2F).
+- [Download the beta examples](https://cdn.docsie.io/file/workspace_u4AEu22YJT50zKF8J/doc_VDWGWsJAhd453cYSI/boo_KXghxr3Yqvg6QlfuD/file_OaxHN7aj5f8twxWoh/c_c_ljm_2021-08-20_0.zip)
 
 
-== LabJack T7 ==
+## LabJack T7 Python
 
-The T7 uses a different python library, apparently a wrapper to the LJM C
-library, called labjack-ljm-python.  It can be installed with pip.
+The T7 uses a different python library from the [LabJack U6](LabJackU6.md),
+apparently a wrapper to the LJM C library, called `labjack-ljm-python`.  It
+can be installed with pip.
 
 ```
 pip install labjack-ljm
 ```
-
-Examples are on this page: https://labjack.com/pages/support?doc=%2Fsoftware-driver%2Fexample-codewrappers%2Fcc-for-ljm-windows-mac-linux%2F
-
-Link to download the beta examples:
-
-https://cdn.docsie.io/file/workspace_u4AEu22YJT50zKF8J/doc_VDWGWsJAhd453cYSI/boo_KXghxr3Yqvg6QlfuD/file_OaxHN7aj5f8twxWoh/c_c_ljm_2021-08-20_0.zip
-
--- test_t7.cc
+++ test_t7.cc
@@ -114,104 +114,6 @@ int CountAndOutputNumSkippedScans(int numInChannels, int scansPerRead, double * 
 }
 
 
-std::string
-device_info_string(int handle)
-{
-    int DeviceType, ConnectionType, SerialNumber, IPAddress, Port,
-        MaxBytesPerMB;
-
-    std::ostringstream msg;
-
-    int err = LJM_GetHandleInfo(handle, &DeviceType, &ConnectionType,
-        &SerialNumber, &IPAddress, &Port, &MaxBytesPerMB);
-
-    if (err)
-    {
-        PLOG(("GetHandleInfo(") << handle << ") failed: "
-              << ljm_error_to_string(err));
-        return "";
-    }
-
-    char ipAddressString[LJM_IPv4_STRING_SIZE];
-
-    // Print
-    msg << "deviceType: " << NumberToDeviceType(DeviceType);
-    msg << "; connectionType: " << NumberToConnectionType(ConnectionType);
-    msg << "; serialNumber: " << SerialNumber;
-
-    LJM_NumberToIP(IPAddress, ipAddressString);
-    msg << "; IP address: " << ipAddressString;
-    msg << "; pipe: " << Port;
-
-    msg << "; max bytes per packet: " << MaxBytesPerMB;
-    return msg.str();
-}
-
-
-
-
-
-void Stream(int handle, int numChannels, const char ** channelNames,
-    double scanRate, int scansPerRead, int numReads);
-
-void HardcodedConfigureStream(int handle);
-
-NidasApp app("test_t7");
-
-
-int main(int argc, char const *argv[])
-{
-    Logger* logger = Logger::getInstance();
-    LogConfig lc("info");
-    logger->setScheme(logger->getScheme("default").addConfig(lc));
-
-    app.enableArguments(app.Help | app.loggingArgs());
-    app.parseArgs(argc, argv);
-    if (app.helpRequested())
-    {
-        std::cout << "Usage: " << argv[0] << " [options] \n";
-        std::cout << app.usage();
-        return 0;
-    }
-
-    // How fast to stream in Hz
-    double INIT_SCAN_RATE = 2000;
-
-    // How many scans to get per call to LJM_eStreamRead. INIT_SCAN_RATE/2 is
-    // recommended
-    int SCANS_PER_READ = INIT_SCAN_RATE / 2;
-
-    // How many times to call LJM_eStreamRead before calling LJM_eStreamStop
-    const int NUM_READS = 10;
-
-    // Channels/Addresses to stream. NUM_CHANNELS can be less than or equal to
-    // the size of CHANNEL_NAMES
-    enum { NUM_CHANNELS = 2 };
-    const char * CHANNEL_NAMES[] = {"AIN0", "AIN1"};
-
-    // Open first found LabJack
-    int handle, err;
-    err = LJM_Open(LJM_dtT7, LJM_ctUSB, "LJM_idANY", &handle);
-    if (err)
-    {
-        std::cerr << "open(T7, USB) failed: " << ljm_error_to_string(err)
-                  << std::endl;
-        return 1;
-    }
-
-    ILOG(("") << device_info_string(handle));
-
-    Stream(handle, NUM_CHANNELS, CHANNEL_NAMES, INIT_SCAN_RATE, SCANS_PER_READ,
-        NUM_READS);
-
-    err = LJM_Close(handle);
-    if (err)
-        PLOG(("") << "Close(" << handle << ") failed: " << ljm_error_to_string(err));
-
-    return 0;
-}
-
-
 void
 set_name(int handle, const std::string& name, double value)
 {
@@ -226,7 +128,134 @@ set_name(int handle, const std::string& name, double value)
 }
 
 
-void HardcodedConfigureStream(int handle)
+/**
+ * HotFilm encapsulates the settings to stream hot film anemometer channels
+ * from the LabJack T7 ADC and record them to disk.
+ */
+class HotFilm
+{
+public:
+    // HotFilm();
+
+    // LJM library handle for the T7 device
+    int handle = -1;
+
+    // How fast to stream in Hz
+    double INIT_SCAN_RATE = 2000;
+
+    // How many scans to get per call to LJM_eStreamRead. INIT_SCAN_RATE/2 is
+    // recommended
+    int SCANS_PER_READ = INIT_SCAN_RATE / 2;
+
+    // How many times to call LJM_eStreamRead before calling LJM_eStreamStop
+    int NUM_READS = 10;
+
+    // Channels/Addresses to stream.
+    std::vector<std::string> channel_names{"AIN0", "AIN1", "AIN2", "AIN3"};
+
+    int DeviceType = -1;
+    int ConnectionType = -1;
+    int SerialNumber = 0;
+    int IPAddress = 0;
+    int Port = 0;
+    int MaxBytesPerMB = 0;
+
+    // Addresses for the channels.
+    vector<int> aScanList;
+    vector<int> aScanTypes;
+
+    // data storage
+    vector<double> aData;
+
+    int open();
+
+    int close();
+
+    int get_handle_info();
+
+    int get_channel_addresses();
+
+    void configure_stream();
+
+    void stream();
+
+    std::string device_info();
+};
+
+
+
+int HotFilm::open()
+{
+    // Open first found LabJack
+    int err = LJM_Open(LJM_dtT7, LJM_ctUSB, "LJM_idANY", &handle);
+    check_error(err, "open(T7, USB)");
+    if (err)
+        return err;
+
+    if ((err = get_handle_info()))
+        return err;
+    ILOG(("") << device_info());
+    return LJME_NOERROR;
+}
+
+
+int HotFilm::close()
+{
+    int err = LJM_Close(handle);
+    handle = -1;
+    check_error(err, "close()");
+    return err;
+}
+
+
+int HotFilm::get_handle_info()
+{
+    int err = LJM_GetHandleInfo(handle, &DeviceType, &ConnectionType,
+        &SerialNumber, &IPAddress, &Port, &MaxBytesPerMB);
+    check_error(err, "GetHandleInfo()");
+    return err;
+}
+
+
+std::string
+HotFilm::device_info()
+{
+    char ipAddressString[LJM_IPv4_STRING_SIZE];
+
+    std::ostringstream msg;
+    msg << "deviceType: " << NumberToDeviceType(DeviceType);
+    msg << "; connectionType: " << NumberToConnectionType(ConnectionType);
+    msg << "; serialNumber: " << SerialNumber;
+
+    LJM_NumberToIP(IPAddress, ipAddressString);
+    msg << "; IP address: " << ipAddressString;
+    msg << "; pipe: " << Port;
+
+    msg << "; max bytes per packet: " << MaxBytesPerMB;
+    return msg.str();
+}
+
+
+int
+HotFilm::
+get_channel_addresses()
+{
+    // cache the addresses for the channel names
+    unsigned int nchannels = channel_names.size();
+    const char* names[nchannels];
+    for (unsigned int i = 0; i < nchannels; ++i)
+        names[i] = channel_names[i].c_str();
+    aScanList.resize(nchannels);
+    aScanTypes.resize(nchannels);
+    int err = LJM_NamesToAddresses(channel_names.size(), names, aScanList.data(), aScanTypes.data());
+    check_error(err, "Getting positive channel addresses");
+    return err;
+}
+
+
+void
+HotFilm::
+configure_stream()
 {
     const int STREAM_TRIGGER_INDEX = 0;
     const int STREAM_CLOCK_SOURCE = 0;
@@ -235,15 +264,17 @@ void HardcodedConfigureStream(int handle)
     const double AIN_ALL_RANGE = 0;
     const int AIN_ALL_NEGATIVE_CH = LJM_GND;
 
+    get_channel_addresses();
+
     ILOG(("Writing configurations..."));
 
     if (STREAM_TRIGGER_INDEX == 0) {
-        ILOG(("    Ensuring triggered stream is disabled:"));
+        ILOG(("ensuring triggered stream is disabled:"));
     }
     set_name(handle, "STREAM_TRIGGER_INDEX", STREAM_TRIGGER_INDEX);
 
     if (STREAM_CLOCK_SOURCE == 0) {
-        ILOG(("    Enabling internally-clocked stream:"));
+        ILOG(("enabling internally-clocked stream:"));
     }
     set_name(handle, "STREAM_CLOCK_SOURCE", STREAM_CLOCK_SOURCE);
 
@@ -258,61 +289,90 @@ void HardcodedConfigureStream(int handle)
     set_name(handle, "AIN_ALL_NEGATIVE_CH", AIN_ALL_NEGATIVE_CH);
 }
 
-void Stream(int handle, int numChannels, const char ** channelNames,
-    double scanRate, int scansPerRead, int numReads)
+
+int main(int argc, char const *argv[])
 {
-    int err, iteration, channel;
+    NidasApp app("test_t7");
+    Logger* logger = Logger::getInstance();
+    LogConfig lc("info");
+    logger->setScheme(logger->getScheme("default").addConfig(lc));
+
+    app.enableArguments(app.Help | app.loggingArgs());
+    app.parseArgs(argc, argv);
+    if (app.helpRequested())
+    {
+        std::cout << "Usage: " << argv[0] << " [options] \n";
+        std::cout << app.usage();
+        return 0;
+    }
+
+    HotFilm hf;
+
+    try {
+        hf.open();
+        hf.configure_stream();
+        hf.stream();
+        hf.close();
+    }
+    catch (std::runtime_error& re)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+void
+HotFilm::
+stream()
+{
+    int err, iteration;
+    unsigned int channel;
     int numSkippedScans = 0;
     int totalSkippedScans = 0;
     int deviceScanBacklog = 0;
     int LJMScanBacklog = 0;
     unsigned int receiveBufferBytesSize = 0;
     unsigned int receiveBufferBytesBacklog = 0;
-    int connectionType;
 
-    vector<int> aScanList(numChannels, 0);
+    unsigned int numChannels = channel_names.size();
+    unsigned int aDataSize = numChannels * SCANS_PER_READ;
+    aData.resize(aDataSize);
 
-    unsigned int aDataSize = numChannels * scansPerRead;
-    vector<double> aData(aDataSize, 0);
+    auto scanRate = INIT_SCAN_RATE;
+    auto numReads = NUM_READS;
+    auto scansPerRead = SCANS_PER_READ;
 
-    err = LJM_GetHandleInfo(handle, NULL, &connectionType, NULL, NULL, NULL, NULL);
-    check_error(err, "GetHandleInfo: ");
-
-    err = LJM_NamesToAddresses(numChannels, channelNames, aScanList.data(), NULL);
-    check_error(err, "Getting positive channel addresses");
-
-    HardcodedConfigureStream(handle);
-
-    printf("\n");
-    printf("Starting stream...\n");
+    ILOG(("Starting stream, %d scans per read, %d channels, "
+          "requesting scan rate %.02f...",
+          scansPerRead, numChannels, scanRate));
     err = LJM_eStreamStart(handle, scansPerRead, numChannels, aScanList.data(),
-        &scanRate);
+                           &scanRate);
     check_error(err, "LJM_eStreamStart");
-    printf("Stream started. Actual scan rate: %.02f Hz (%.02f sample rate)\n",
-        scanRate, scanRate * numChannels);
-    printf("\n");
+    ILOG(("Stream started. Actual scan rate: %.02f Hz (%.02f sample rate)",
+          scanRate, scanRate * numChannels));
 
     // Read the scans
-    printf("Now performing %d reads\n", numReads);
-    printf("\n");
+    ILOG(("Now performing %d reads", numReads));
     for (iteration = 0; iteration < numReads; iteration++) {
         err = LJM_eStreamRead(handle, aData.data(), &deviceScanBacklog,
             &LJMScanBacklog);
         check_error(err, "LJM_eStreamRead");
 
-        printf("iteration: %d - deviceScanBacklog: %d, LJMScanBacklog: %d",
-            iteration, deviceScanBacklog, LJMScanBacklog);
-        if (connectionType != LJM_ctUSB) {
+        ILOG(("iteration: %d - deviceScanBacklog: %d, LJMScanBacklog: %d",
+              iteration, deviceScanBacklog, LJMScanBacklog));
+        if (ConnectionType != LJM_ctUSB) {
             err = LJM_GetStreamTCPReceiveBufferStatus(handle,
                 &receiveBufferBytesSize, &receiveBufferBytesBacklog);
             check_error(err, "LJM_GetStreamTCPReceiveBufferStatus");
-            printf(", receive backlog: %f%%",
-                ((double)receiveBufferBytesBacklog) / receiveBufferBytesSize * 100);
+            ILOG(("-> receive backlog: %f%%",
+                 ((double)receiveBufferBytesBacklog) / receiveBufferBytesSize * 100));
         }
         printf("\n");
         printf("  1st scan out of %d:\n", scansPerRead);
         for (channel = 0; channel < numChannels; channel++) {
-            printf("    %s = %0.5f\n", channelNames[channel], aData[channel]);
+            printf("    %s = %0.5f\n", channel_names[channel].c_str(), aData[channel]);
         }
 
         numSkippedScans = CountAndOutputNumSkippedScans(numChannels,
@@ -330,7 +390,7 @@ void Stream(int handle, int numChannels, const char ** channelNames,
             totalSkippedScans);
     }
 
-    printf("Stopping stream\n");
+    ILOG(("Stopping stream"));
     err = LJM_eStreamStop(handle);
     check_error(err, "Stopping stream");
 }
