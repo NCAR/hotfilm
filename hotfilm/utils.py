1aaf26f7c9c9a5bdd62ccaea2ea74e58a85dc9ae
Gary Granger <granger@ucar.edu>
2025-03-17 08:01:57 -0600

    allow conversion of time coordinate to seconds...
    and not just microseconds.

--- hotfilm/utils.py
+++ hotfilm/utils.py
@@ -6,30 +6,42 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-_microseconds_per_seconds = 1000000
-_microseconds_per_day = 24*60*60*_microseconds_per_seconds
+_seconds_per_day = 24*60*60
+_microseconds_per_second = 1000000
 
 
 def td_to_microseconds(td64: np.timedelta64) -> int:
     td = pd.to_timedelta(td64)
-    return (td.days * _microseconds_per_day +
-            td.seconds * _microseconds_per_seconds +
+    return ((td.days * _seconds_per_day +
+             td.seconds) * _microseconds_per_second +
             td.microseconds)
 
 
-def convert_time_coordinate(ds: xr.Dataset, dt: xr.DataArray) -> xr.Dataset:
+def td_to_seconds(td64: np.timedelta64) -> int:
+    td = pd.to_timedelta(td64)
+    return (td.days * _seconds_per_day) + td.seconds
+
+
+def convert_time_coordinate(ds: xr.Dataset, dt: xr.DataArray,
+                            ustep: str = "microseconds") -> xr.Dataset:
     """
-    Convert a time coordinate in a Dataset to int64 microseconds since the
-    first time in the coordinate array.
+    Convert a time coordinate in a Dataset to int64 microseconds or seconds
+    since the first time in the coordinate array.  @p ustep is "seconds" or
+    "microseconds".
     """
     # numerous and varied attempts failed to get xarray to encode
     # the time as microseconds since base, so do it manually.
     when = pd.to_datetime(dt.data[0])
     base = when.replace(microsecond=0)
-    units = ('microseconds since %s' %
+    units = (f'{ustep} since %s' %
              base.strftime("%Y-%m-%d %H:%M:%S+00:00"))
     base = np.datetime64(base)
-    vtime = np.array([td_to_microseconds(t) for t in (dt - base).data],
+    convert = td_to_seconds
+    if ustep == 'microseconds':
+        convert = td_to_microseconds
+    elif ustep != 'seconds':
+        raise ValueError(f"unknown time unit: {ustep}")
+    vtime = np.array([convert(t) for t in (dt - base).data],
                      dtype='int64')
     ds = ds.assign_coords({dt.name: vtime})
     ds[dt.name].attrs.update(dt.attrs)
