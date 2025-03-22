a2ee38b7bd309188f1dbcaa2d3aca90e63bf4347
Gary Granger <granger@ucar.edu>
2025-03-22 15:03:44 -0600

    start a script to run all processing for specific dates

---
+++ run_dates.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# make sure right mamba environment and nidas path are active
+# mamba activate hotfilm
+# source /opt/nidas/bin/setup_nidas.sh
+
+# Dates of interest
+dates=read <<EOF
+20230804
+20230827
+20230905
+20230915
+20230919
+20230920
+20230922
+EOF
+
+date=20230804
+
+# Output will go into subdirectories of the current directory by default.
+
+hotfilmdir=/opt/local/m2hats/hotfilm
+dataroot=/scr/isfs/projects/M2HATS
+rawdata=${dataroot}/raw_data
+sonics=/export/flash/isf/isfs/data/M2HATS/20250113/hr_qc_instrument/
+
+hotfilm_output=./hotfilm
+hotfilm_output_spec=${hotfilm_output}/hotfilm_%Y%m%d_%H%M%S.nc
+calibrated_output=./windspeed/$date
+calibrated_output_spec=${calibrated_output}/hotfilm_wind_speed_%Y%m%d_%H%M%S.nc
+plot_output=./windspeed/$date/plots
+plot_output_spec=${plot_output}/hotfilm_calibrations_%Y%m%d_%H%M%S.png
+
+dumphotfilm="${hotfilmdir}/dump_hotfilm.py --netcdf ${hotfilm_output_spec} --log info"
+calibrate="${hotfilmdir}/calibrate_hotfilm.py --log info --sonics ${sonics} --plot --calibrate --netcdf ${calibrated_output_spec} --images ${plot_output_spec}"
+
+
+get_inputs()
+{
+    echo "$@" | awk '{ print $1; }'
+}
+
+
+set -x
+mkdir -p ${hotfilm_output}
+mkdir -p ${calibrated_output}
+mkdir -p ${plot_output}
+$dumphotfilm ${rawdata}/hotfilm_${date}_*.dat
+$calibrate ${hotfilm_output}/hotfilm_${date}_*.nc
