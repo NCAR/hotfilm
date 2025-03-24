4532c2a42c9396de105fded8df2748fa96a101a0
Gary Granger <granger@ucar.edu>
2025-03-23 20:04:42 -0600

    refactor into functions, run all dates in parallel

--- run_dates.sh
+++ run_dates.sh
@@ -23,6 +23,8 @@ hotfilmdir=/opt/local/m2hats/hotfilm
 dataroot=/scr/isfs/projects/M2HATS
 rawdata=${dataroot}/raw_data
 sonics=/export/flash/isf/isfs/data/M2HATS/20250113/hr_qc_instrument/
+speed_spec=hotfilm_wind_speed_%Y%m%d_%H%M%S.nc
+plot_spec=hotfilm_calibrations_%Y%m%d_%H%M%S.png
 
 hotfilm_output=./hotfilm
 hotfilm_output_spec=${hotfilm_output}/hotfilm_%Y%m%d_%H%M%S.nc
@@ -31,8 +33,8 @@ calibrated_output_spec=${calibrated_output}/hotfilm_wind_speed_%Y%m%d_%H%M%S.nc
 plot_output=./windspeed/$date/plots
 plot_output_spec=${plot_output}/hotfilm_calibrations_%Y%m%d_%H%M%S.png
 
-dumphotfilm="${hotfilmdir}/dump_hotfilm.py --netcdf ${hotfilm_output_spec} --log info"
-calibrate="${hotfilmdir}/calibrate_hotfilm.py --log info --sonics ${sonics} --plot --calibrate --netcdf ${calibrated_output_spec} --images ${plot_output_spec}"
+dumphotfilm="${hotfilmdir}/dump_hotfilm.py --log info"
+calibrate="${hotfilmdir}/calibrate_hotfilm.py --log info --sonics ${sonics} --plot --calibrate"
 
 
 get_inputs()
@@ -41,9 +43,42 @@ get_inputs()
 }
 
 
-set -x
-mkdir -p ${hotfilm_output}
-mkdir -p ${calibrated_output}
-mkdir -p ${plot_output}
-$dumphotfilm ${rawdata}/hotfilm_${date}_*.dat
-$calibrate ${hotfilm_output}/hotfilm_${date}_*.nc
+run_dump() # date
+{
+    date="$1"
+    set -x
+    mkdir -p ${hotfilm_output}
+    $dumphotfilm --netcdf ${hotfilm_output_spec} \
+        ${rawdata}/hotfilm_${date}_*.dat
+}
+
+
+run_calibrate() # date
+{
+    date="$1"
+    calibrated_output=./windspeed/$date
+    calibrated_output_spec=${calibrated_output}/${speed_spec}
+    plot_output=./windspeed/$date/plots
+    plot_output_spec=${plot_output}/${plot_spec}
+
+    set -x
+    mkdir -p ${calibrated_output}
+    mkdir -p ${plot_output}
+    $calibrate --netcdf ${calibrated_output_spec} \
+        --images ${plot_output_spec} \
+        ${hotfilm_output}/hotfilm_${date}_*.nc
+}
+
+
+run_date() # date
+{
+    date="$1"
+    run_dump $date
+    run_calibrate $date
+}
+
+for dt in $dates ; do
+    run_date $dt >& run_dates.${date}.log &
+done
+
+wait
