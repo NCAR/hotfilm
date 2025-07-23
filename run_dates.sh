914f242d510cd640fdea5ea658d9aa23d1eeb576
Gary Granger <granger@ucar.edu>
2025-07-23 16:12:25 -0600

    allow sonic data to be staged separately
    ie, to dump hotfilm and stage sonic data without calibrating

--- run_dates.sh
+++ run_dates.sh
@@ -10,6 +10,7 @@ hotfilmdir=/opt/local/m2hats/hotfilm
 dataroot=/scr/isfs/projects/M2HATS
 rawdata=${dataroot}/raw_data
 sonics=/export/flash/isf/isfs/data/M2HATS/20250113/hr_qc_instrument/
+local_sonics="hr_qc_instrument"
 speed_spec=hotfilm_wind_speed_%Y%m%d_%H%M%S.nc
 plot_spec=hotfilm_calibrations_%Y%m%d_%H%M%S.png
 
@@ -24,6 +25,7 @@ dumphotfilm="${hotfilmdir}/dump_hotfilm.py --log info"
 calibrate="${hotfilmdir}/calibrate_hotfilm.py --log info --plot --calibrate"
 
 do_dump=0
+do_stage=0
 do_calibrate=0
 dates=""
 
@@ -55,24 +57,31 @@ run_dump() # date
 }
 
 
-run_calibrate() # date
+stage_sonic_data() # date
 {
     date="$1"
-    echo "Calibrating hotfilm data for $date ..."
-    calibrated_output=./windspeed/$date
-    calibrated_output_spec=${calibrated_output}/${speed_spec}
-    plot_output=./windspeed/$date/plots
-    plot_output_spec=${plot_output}/${plot_spec}
-
+    echo "Staging sonic data for $date ..."
     # make sure corresponding sonic data files are copied into this run
     # directory.  I'm not sure if reading the sonic data from the local
     # copy is any slower than reading directly from the flash filesystem,
     # but this at least makes the runs more self-contained, and this
     # includes the sonic data in the final output when linked to the web
     # filesystem.
-    local_sonics="hr_qc_instrument"
     mkdir -p "${local_sonics}"
-    rsync -av $sonics/isfs_m2hats_qc_hr_inst_${date}_*.nc "${local_sonics}/"
+    (set -x; rsync -av $sonics/isfs_m2hats_qc_hr_inst_${date}_*.nc \
+     "${local_sonics}/")
+}
+
+
+run_calibrate() # date
+{
+    date="$1"
+    stage_sonic_data $date
+    echo "Calibrating hotfilm data for $date ..."
+    calibrated_output=./windspeed/$date
+    calibrated_output_spec=${calibrated_output}/${speed_spec}
+    plot_output=./windspeed/$date/plots
+    plot_output_spec=${plot_output}/${plot_spec}
 
     set -x
     mkdir -p ${calibrated_output}
@@ -91,6 +100,9 @@ run_date() # date
     if [ $do_dump -ne 0 ] ; then
         run_dump $date
     fi
+    if [ $do_stage -ne 0 ] ; then
+        stage_sonic_data $date
+    fi
     if [ $do_calibrate -ne 0 ] ; then
         run_calibrate $date
     fi
@@ -105,6 +117,10 @@ while [[ $# -gt 0 ]] ; do
             ;;
         --calibrate)
             do_calibrate=1
+            shift
+            ;;
+        --stage)
+            do_stage=1
             shift
             ;;
         --dates)
@@ -122,7 +138,7 @@ while [[ $# -gt 0 ]] ; do
     esac
 done
 
-if [ $do_dump -eq 0 ] && [ $do_calibrate -eq 0 ] ; then
+if [ $do_dump -eq 0 ] && [ $do_calibrate -eq 0 ] && [ $do_stage -eq 0 ]; then
     do_dump=1
     do_calibrate=1
 fi
